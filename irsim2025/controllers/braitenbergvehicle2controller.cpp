e4378217974f8de8592e856d2e0e8ddccc70ed2a
mmooppssoo <agruizesc@gmail.com>
2025-02-26 10:12:29 +0000

    Braitenberg aproximacion+giro alrededor de la luz

--- irsim2025/controllers/braitenbergvehicle2controller.cpp
+++ irsim2025/controllers/braitenbergvehicle2controller.cpp
@@ -78,195 +78,94 @@ CBraitenbergVehicle2Controller::~CBraitenbergVehicle2Controller()
 
 void CBraitenbergVehicle2Controller::SimulationStep(unsigned n_step_number, double f_time, double f_step_interval)
 {
-
-
-	/* FASE 1: LECTURA DE SENSORES */
-
-	/* Leer Sensores de Contacto */
-	double* contact = m_seContact->GetSensorReading(m_pcEpuck);
-	/* Leer Sensores de Proximidad */
-	double* prox = m_seProx->GetSensorReading(m_pcEpuck);
-	/* Leer Sensores de Luz */
+	/* Read Light */
 	double* light = m_seLight->GetSensorReading(m_pcEpuck);
-	/* Leer Sensores de Luz Azul*/
-	double* bluelight = m_seBlueLight->GetSensorReading(m_pcEpuck);
-	/* Leer Sensores de Luz Roja*/
-	double* redlight = m_seRedLight->GetSensorReading(m_pcEpuck);
-	/* Leer Sensores de Suelo */
-	double* ground = m_seGround->GetSensorReading(m_pcEpuck);
-	/* Leer Sensores de Suelo Memory */
-	double* groundMemory = m_seGroundMemory->GetSensorReading(m_pcEpuck);
-	/* Leer Battery Sensores de Suelo Memory */
-	double* battery = m_seBattery->GetSensorReading(m_pcEpuck);
-	/* Leer Blue Battery Sensores de Suelo Memory */
-	double* bluebattery = m_seBlueBattery->GetSensorReading(m_pcEpuck);
-	/* Leer Red Battery Sensores de Suelo Memory */
-	double* redbattery = m_seRedBattery->GetSensorReading(m_pcEpuck);
 
-	
-	/* FASE 2: CONTROLADOR */
-	
-	/* Inicio Incluir las ACCIONES/CONTROLADOR a implementar */
-	//printf("CONTACT: ");
-	//for ( int i = 0 ; i < m_seContact->GetNumberOfInputs() ; i ++ )
-	//{
-		//printf("%1.3f ", contact[i]);
-	//}
-	//printf("\n");
-	
-	//printf("PROX: ");
-	//for ( int i = 0 ; i < m_seProx->GetNumberOfInputs() ; i ++ )
-	//{
-		//printf("%1.3f ", prox[i]);
-	//}
-	//printf ("\n");
-	
-  printf("LIGHT: ");
-  for ( int i = 0 ; i < m_seLight->GetNumberOfInputs() ; i ++ )
-  {
+	/* Print Light */	
+	printf("LIGHT: ");
+	for ( int i = 0 ; i < m_seLight->GetNumberOfInputs() ; i ++ ){
     printf("%1.3f ", light[i]);
-  }
-  printf ("\n");
-	
-	//printf("BLUE LIGHT: ");
-	//for ( int i = 0 ; i < m_seBlueLight->GetNumberOfInputs() ; i ++ )
-	//{
-		//printf("%1.3f ", bluelight[i]);
-	//}
-	//printf ("\n");
-	
-	//printf("RED LIGHT: ");
-	//for ( int i = 0 ; i < m_seRedLight->GetNumberOfInputs() ; i ++ )
-	//{
-		//printf("%1.3f ", redlight[i]);
-	//}
-	//printf ("\n");
-	
-	//printf("GROUND: ");
-	//for ( int i = 0 ; i < m_seGround->GetNumberOfInputs() ; i ++ )
-	//{
-		//printf("%1.3f ", ground[i]);
-	//}
-	//printf("\n");
+    }
+    printf ("\n");
 
-	//printf("GROUND MEMORY: ");
-	//for ( int i = 0 ; i < m_seGroundMemory->GetNumberOfInputs() ; i ++ )
-	//{
-		//printf("%1.3f ", groundMemory[i]);
-	//}
-	//printf("\n");
-	
-	//printf("BATTERY: ");
-	//for ( int i = 0 ; i < m_seBattery->GetNumberOfInputs() ; i ++ )
-	//{
-		//printf("%1.3f ", battery[i]);
-	//}
-	//printf("\n");
-	
-	//printf("BLUE BATTERY: ");
-	//for ( int i = 0 ; i < m_seBlueBattery->GetNumberOfInputs() ; i ++ )
-	//{
-		//printf("%1.3f ", bluebattery[i]);
-	//}
-	//printf("\n");
-	//printf("RED BATTERY: ");
-	//for ( int i = 0 ; i < m_seRedBattery->GetNumberOfInputs() ; i ++ )
-	//{
-		//printf("%1.3f ", redbattery[i]);
-	//}
-	//printf("\n");
-
-	/* Fin: Incluir las ACCIONES/CONTROLADOR a implementar */
-
-
-	FILE* filePosition = fopen("outputFiles/robotPosition", "a");
-	fprintf(filePosition," %2.4f %2.4f %2.4f %2.4f\n",
-	f_time, m_pcEpuck->GetPosition().x,
-	m_pcEpuck->GetPosition().y,
-	m_pcEpuck->GetRotation());
-	fclose(filePosition);
-	
-	
-
-	/* Fase 3: ACTUACIÓN */
-
-  /* TEST 1-8: Light (-0.7, 0.7), Robot (0.0,0.0,1.57) */
-  /* Test 1: V2 - Contra Lateral + Light */
-  //m_acWheels->SetOutput(0, 0.5 + (light[7] ));
-  //m_acWheels->SetOutput(1, 0.5 + (light[0] ));
+    /* Define speed for left and right wheel */
+    double speed[2];
   
-  /* Test 2: V2 - Lateral + Light */
-  //m_acWheels->SetOutput(0, 0.5 + (light[0] ));
-  //m_acWheels->SetOutput(1, 0.5 + (light[7] ));
+    /* STUDENTS MUST IMPLEMENT THE CODE HERE - START 
+    * IMPORTANT!!: The code must exclusively depend on "light" variable */
+	double tmp[2];
+	static int giro = 0;
 
-  /* Test 3: V2 - Contra Lateral all + light */
-  //double tmp[2];
+	tmp[0] = light[0] + light[1] + light[2] + light[3]; // Lado izquierdo
+	tmp[1] = light[4] + light[5] + light[6] + light[7]; // Lado derecho
 
-  //tmp[0] = light[0] + light[1] + light[2] + light[3];
-  //tmp[1] = light[4] + light[5] + light[6] + light[7];
+	double light_total = tmp[0] + tmp[1];
 
-  //m_acWheels->SetOutput(0, 0.5 + (tmp[1]));
-  //m_acWheels->SetOutput(1, 0.5 + (tmp[0]));
-  
-  /* Test 4: V2 - Lateral all + light */
-  //double tmp[2];
-
-  //tmp[0] = light[0] + light[1] + light[2] + light[3];
-  //tmp[1] = light[4] + light[5] + light[6] + light[7];
-
-  //m_acWheels->SetOutput(0, 0.5 + (tmp[0]));
-  //m_acWheels->SetOutput(1, 0.5 + (tmp[1]));
-
-  /* Test 5: V2 - Lateral - Light */
-	/* Stops with 0.7 and unstable with 0.75 */
-  //m_acWheels->SetOutput(0, 0.5 + (0.5 - light[0] ));
-  //m_acWheels->SetOutput(1, 0.5 + (0.5 - light[7] ));
-  
-  /* Test 6: V2 - Contra-Lateral - Light */
-  //m_acWheels->SetOutput(0, 0.5 + (0.5 - light[7] ));
-  //m_acWheels->SetOutput(1, 0.5 + (0.5 - light[0] ));
-
-  /* Test 7: V2 - Contra-Lateral all - Light */
-  //double tmp[2];
-
-  //tmp[0] = light[0] + light[1] + light[2] + light[3];
-  //tmp[1] = light[4] + light[5] + light[6] + light[7];
-
-  //m_acWheels->SetOutput(0, 0.5 + (0.5 - tmp[1] ));
-  //m_acWheels->SetOutput(1, 0.5 + (0.5 - tmp[0] ));
-
-  /* Test 8: V2 - Lateral all - Light */
-  //double tmp[2];
-
-  //tmp[0] = light[0] + light[1] + light[2] + light[3];
-  //tmp[1] = light[4] + light[5] + light[6] + light[7];
-
-  //m_acWheels->SetOutput(0, 0.5 + (0.95 - tmp[0] ));
-  //m_acWheels->SetOutput(1, 0.5 + (0.95 - tmp[1] ));
+	if ((light_total >= 1.55 && light_total <= 1.65) || giro == 1) {
+    	giro = 1;
+    
+    	// Detectar el sensor con mayor intensidad de luz
+    	double max_light = 0;
+    	int max_light_index = 0;
+    	for (int i = 0; i < 8; i++) {
+        	if (light[i] > max_light) {
+            	max_light = light[i];
+            	max_light_index = i;
+        	}
+    	}
+    
+    	// Ajuste de velocidad basado en la posicion del sensor con mayor luz
+    	if (max_light_index == 0) { 
+        	speed[0] = 0.9;  
+        	speed[1] = 0.6;  
+    	}
+		else if (max_light_index == 1) { 
+        	speed[0] = 0.8;  
+        	speed[1] = 0.7;  
+    	}
+		else if (max_light_index == 2) { 
+        	speed[0] = 0.7;  
+        	speed[1] = 0.8;  
+    	}
+		else if (max_light_index == 7) { 
+        	speed[0] = 0.6;  
+        	speed[1] = 0.9;  
+    	}
+    	else if (max_light_index == 6) { 
+        	speed[0] = 0.7;  
+        	speed[1] = 0.8;  
+    	}
+    	else if (max_light_index == 5) { 
+        	speed[0] = 0.8;  
+        	speed[1] = 0.7;  
+    	}
+    	else if (max_light_index == 3 || max_light_index == 4) { 
+        	speed[0] = 0.9;  
+        	speed[1] = 0.9;  
+    	}
 	
-  /* TEST 9: Light (-0.7, 0.7) & (0.7, -0.7), Robot (0.0,0.0,1.57) */
-  /* Test 9: V4 - Back&Forth - Light */
-  //double tmp[2];
+	} else if (giro == 0) {
+		if (((light[3] > light[4]) && (light[3] - light[4] ) < 0.150) && light[3] > 0.3){
+			speed[0] = 0.5;
+    		speed[1] = 1;
+		}
+		else if (((light[3] < light[4]) && (light[4] - light[3]) < 0.150) && light[4] > 0.3){
+			speed[0] = 1;
+    		speed[1] = 0.5;
+		}
+    	else {
+			// Vehiculo de Braitenberg carinoso
+    		speed[0] = 0.5 + (0.95 - tmp[0]);
+    		speed[1] = 0.5 + (0.95 - tmp[1]);
+		}
+	}
 
-  //tmp[0] = light[0] + light[1]; // + light[2];
-  //tmp[1] = light[7] + light[6]; // + light[5];
-
-  //m_acWheels->SetOutput(0, (tmp[1] ));
-  //m_acWheels->SetOutput(1, (tmp[0] ));
-
-  /* TEST 9: Light (-0.7, 0.7) & (0.7, -0.7), Robot (0.0,0.0,1.57) */
-  /* Test 9: V4 - Back&Forth - Light */
-  double tmp[2];
-
-  tmp[0] = light[0] + light[1]; // + light[2];
-  tmp[1] = light[7] + light[6]; // + light[5];
-
-  if (tmp[1] > 0.5)
-    tmp[1] = 1 - tmp[1];
-
-
-  m_acWheels->SetOutput(0, 0.6 + (tmp[1] ));
-  m_acWheels->SetOutput(1, 0.6 + (tmp[0] ));
+  
+  /* STUDENTS MUST IMPLEMENT THE CODE HERE - END*/
+  
+  /* Set speed to wheels */
+  m_acWheels->SetOutput(0, speed[0]);
+  m_acWheels->SetOutput(1, speed[1]);
 }
 
 /******************************************************************************/
